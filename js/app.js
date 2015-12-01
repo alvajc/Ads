6f11a10685dccbc64ab445cc4b52469b91c146c2
Juan Alvarado <alva.juan79@gmail.com>
2015-11-30 16:17:49 -0800

    Hiding elements on ads site

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
+//-------sign up 
 var signUp = document.getElementById('signup');
 
 function showSignUp() {
   signUp.setAttribute('class', 'show');
 }
 
-
-
 var signForm = document.getElementById('signform');
 
+signForm.addEventListener('click', showSignUp, false);
 
-signform.addEventListener('mouseover', showSignUp, false);
+//-----search
+var searchResults = document.getElementById('searchresults')
 
+function showSearchResults() {
+  searchResults.setAttribute('class', 'show');
+}
+
+var searchButton = document.getElementById('searchbutton')
+
+searchButton.addEventListener('click', showSearchResults, false);
 
